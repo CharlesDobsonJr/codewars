294069c314dd83bf59a0c51e659dc2579955cb34
Charles Dobson <charlesmdobsonjr@gmail.com>
2023-07-20 15:35:18 -0400

    Added 2 solutions to 7-20-23 code challenge.

--- 8kyu/do-i-get-a-bonus.js
+++ 8kyu/do-i-get-a-bonus.js
@@ -5,3 +5,22 @@
 // If bonus is true, the salary should be multiplied by 10. If bonus is false, the fatcat did not make enough money and must receive only his stated salary.
 
 // Return the total figure the individual will receive as a string prefixed with "£" (= "\u00A3", JS, Go, Java, Scala, and Julia), "$" (C#, C++, Ruby, Clojure, Elixir, PHP, Python, Haskell, and Lua) or "¥" (Rust).
+
+
+// Solutions
+
+function bonusTime(salary, bonus) {
+    let total;
+    if (bonus) {
+        total = salary * 10;
+    } else {
+        total = salary;
+    }
+    return `£${total}`;
+}
+
+function bonusTime(salary, bonus) {
+    return bonus ? `£${10 * salary}` : `£${salary}`;
+  }
+
+
