b53d080401b714c30dd2dc90f1e4f9500f9cedb3
Charles Dobson <charlesmdobsonjr@gmail.com>
2023-07-28 04:26:51 -0400

    Add coding challenge for 7-28-23 coding challenge.

---
+++ 8kyu/sum-of-positive.js
@@ -0,0 +1,24 @@
+// You get an array of numbers, return the sum of all of the positives ones.
+
+// Example [1,-4,7,12] => 1 + 7 + 12 = 20
+
+// Note: if there is nothing to sum, the sum is default to 0.
+
+// Solutions
+
+function positiveSum(arr) {
+    var total = 0;    
+    for (i = 0; i < arr.length; i++) {    // setup loop to go through array of given length
+      if (arr[i] > 0) {                   // if arr[i] is greater than zero
+        total += arr[i];                  // add arr[i] to total
+      }
+    }
+    return total;                         // return total
+  }
+
+
+  function positiveSum(arr) {
+    return arr.reduce((a,b)=> a + (b > 0 ? b : 0),0);
+ }
+
+ 
