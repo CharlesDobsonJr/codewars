b40521a1f22fcd0dee75cc5364a9a826cf08dd38
Charles Dobson <charlesmdobsonjr@gmail.com>
2023-07-28 04:28:25 -0400

    Added another solution to 7-28-23 code challenge.

--- 8kyu/sum-of-positive.js
+++ 8kyu/sum-of-positive.js
@@ -21,4 +21,6 @@ function positiveSum(arr) {
     return arr.reduce((a,b)=> a + (b > 0 ? b : 0),0);
  }
 
- 
+ function positiveSum (arr) {
+    return arr.filter(x => x>=0).reduce((a, c) => a + c, 0);
+  }
