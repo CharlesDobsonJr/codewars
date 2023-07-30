971a1b3c644a87cd5ca5e3262313a56c6175ae53
Charles Dobson <charlesmdobsonjr@gmail.com>
2023-07-30 04:41:18 -0400

    Added a few solutions to 7-30-23 code challenge.

--- 8kyu/remove-first-and-last-character.js
+++ 8kyu/remove-first-and-last-character.js
@@ -14,3 +14,21 @@ function removeChar(str) {
   function removeChar(str){
     return str.substring(1, str.length-1);
    };
+
+
+   function removeChar(str){
+    //You got this!
+     str1 = str.split('');
+     str1.shift();
+     str1.pop();
+     return str1.join('');
+       
+   };
+
+
+   removeChar = str => str.slice(1,-1)
+
+
+   function removeChar(str){
+    return str.slice(1, str.length - 1);
+  };
