e234108d3101b114faf999d4fe01cf93e579d85d
Charles Dobson <charlesmdobsonjr@gmail.com>
2023-08-02 04:55:57 -0400

    Added code challenge to 8-2-23 code challenge.

---
+++ 8kyu/convert-string-to-number.js
@@ -0,0 +1,45 @@
+// Description
+// We need a function that can transform a string into a number. What ways of achieving this do you know?
+
+// Note: Don't worry, all inputs will be strings, and every string is a perfectly valid representation of an integral number.
+
+// Examples
+// "1234" --> 1234
+// "605"  --> 605
+// "1405" --> 1405
+// "-7" --> -7
+
+
+// Solutions
+
+var stringToNumber = function(str){
+    return parseInt(str);
+  }
+
+
+  var stringToNumber = function(str){
+    // put your code here
+    return Number(str);
+  }
+
+
+  var stringToNumber = function(str){
+    return +str;
+  }
+
+
+  const stringToNumber = str => Number(str)
+
+
+
+  var stringToNumber = function(str){
+    return parseInt(str, 10);
+  }
+
+
+  const stringToNumber = (str) => +str
+
+
+  const stringToNumber = function(str){
+    return Number(str);
+  }
