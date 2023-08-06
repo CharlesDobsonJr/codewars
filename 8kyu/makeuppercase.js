00eab16eea104179ab0819d59bf1457ed493e2e8
Charles Dobson <charlesmdobsonjr@gmail.com>
2023-08-06 05:23:04 -0400

    Added new coding challenge on 8-6-23

---
+++ 8kyu/makeuppercase.js
@@ -0,0 +1,10 @@
+// Write a function which converts the input string to uppercase.
+
+//Solutions
+
+const makeUpperCase = str => str.toUpperCase();
+
+
+function makeUpperCase(str) {return str.toUpperCase();}
+
+
