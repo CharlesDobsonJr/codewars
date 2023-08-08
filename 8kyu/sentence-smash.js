3170ff7dfde77304867e1e80963525776c0f6310
Charles Dobson <charlesmdobsonjr@gmail.com>
2023-08-08 05:33:42 -0400

    Added new challenge for 8-8-23 coding challenge.

---
+++ 8kyu/sentence-smash.js
@@ -0,0 +1,23 @@
+// Write a function that takes an array of words and smashes them together into a sentence and returns the sentence. You can ignore any need to sanitize words or add punctuation, but you should add spaces between each word. Be careful, there shouldn't be a space at the beginning or the end of the sentence!
+
+// Example
+// ['hello', 'world', 'this', 'is', 'great']  =>  'hello world this is great'
+
+//Solutions
+
+smash = function (words) {
+    return words.join(" ");
+  };
+
+
+// Smash Words
+const smash = words => words.join(' ');
+
+
+// Smash Words
+function smash (words) {
+    "use strict";
+    return words.join(' ');
+};
+
+
