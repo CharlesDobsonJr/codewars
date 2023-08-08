6d60a78da00b63227b1e4755319f9cc0241bbe0e
Charles Dobson <charlesmdobsonjr@gmail.com>
2023-08-08 05:30:27 -0400

    Added more solutions to previous code on 8-8-23

--- 8kyu/are-you-playing-banjo.js
+++ 8kyu/are-you-playing-banjo.js
@@ -40,3 +40,10 @@ function areYouPlayingBanjo(name) {
   function areYouPlayingBanjo(name) {
     return (name[0].toLowerCase() === 'r') ? name + ' plays banjo' : name + ' does not play banjo';
   }
+
+
+  function areYouPlayingBanjo(name) {
+    if (name.startsWith("R") || name.startsWith("r")) {
+      return name + " plays banjo";
+    } else return name + " does not play banjo";
+  }
