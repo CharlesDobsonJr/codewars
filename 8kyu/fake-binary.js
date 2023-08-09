2cc6bfd441e2aa6cde63de5c9a6cf35a3e0c0628
Charles Dobson <charlesmdobsonjr@gmail.com>
2023-08-09 05:44:14 -0400

    Added more solutions to 8-9-23 coding challenge.

--- 8kyu/fake-binary.js
+++ 8kyu/fake-binary.js
@@ -27,3 +27,26 @@ function fakeBin(x) {
     }
     return newStr;
   }
+
+
+  function fakeBin(x){
+    return x.replace( /[0-4]/g, "0" ).replace( /[5-9]/g, "1" )
+  }
+
+
+  function fakeBin(x){
+    let result = '';
+    for(let i = 0; i < x.length; i++){
+      x[i] < 5 ? result += 0 : result += 1;
+    }
+    return result;
+  }
+
+  function fakeBin(x){
+    return x.replace(/[1234]/g, '0').replace(/[56789]/g, '1')
+  }
+
+
+  function fakeBin(x){
+    return x.split('').map(a => a < '5' ? "0" : "1").join("");
+  }
