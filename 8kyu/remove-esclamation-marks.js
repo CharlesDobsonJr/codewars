e21e51eadfb2f24d1c722dc57b6c0a8368e6baee
Charles Dobson <charlesmdobsonjr@gmail.com>
2023-08-16 14:04:59 -0400

    Added code challenge 8-16-23 to coding challenge.

---
+++ 8kyu/remove-esclamation-marks.js
@@ -0,0 +1,9 @@
+// Write function RemoveExclamationMarks which removes all exclamation marks from a given string.
+
+// Solutions
+
+function removeExclamationMarks(s) {
+    return s.replace(/!/gi, '');
+  }
+
+
