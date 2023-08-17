ee3673351d75ef8ff6794667ac41475bfe49005c
Charles Dobson <charlesmdobsonjr@gmail.com>
2023-08-17 14:14:05 -0400

    Added more solutions to 8-17-23 code challenge.

--- 8kyu/sum-mixed-array.js
+++ 8kyu/sum-mixed-array.js
@@ -22,4 +22,30 @@ function sumMix(x){
   const sumMix=x=>x.reduce((a,b)=>+b+a,0)
 
 
-  
+  function sumMix(x){
+    var somme = 0;
+    for (i = 0; i<x.length; i++) {
+      somme += Number(x[i]);
+    }
+    return somme;
+  }
+
+
+
+  const sumMix = (arr) => arr.reduce((sum, n) => sum + (+n), 0);
+
+
+
+  function sumMix(x){
+    return x.reduce((s,x)=>s+ +x,0);
+   }
+
+
+   function sumMix(x) {
+    return x.reduce((s, n) => s + Number(n), 0)
+  }
+
+
+  function sumMix(x){
+    return eval( x.join("+") )
+  }
