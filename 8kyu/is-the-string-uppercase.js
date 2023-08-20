97668da9377b2d3014e1b9894ccc31c2eda397af
Charles Dobson <charlesmdobsonjr@gmail.com>
2023-08-20 17:08:31 -0400

    Added a solution to 8-20-23 coding challenge

--- 8kyu/is-the-string-uppercase.js
+++ 8kyu/is-the-string-uppercase.js
@@ -15,3 +15,9 @@ String.prototype.isUpperCase=function() {return this==this.toUpperCase()}
 
 
 
+String.prototype.isUpperCase = function() {
+    return this.toUpperCase() === this.toString();
+  }
+
+
+
