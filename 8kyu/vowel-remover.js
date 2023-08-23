90db17e08e96371b2c85d05f4142a67477ab8843
Charles Dobson <charlesmdobsonjr@gmail.com>
2023-08-23 17:25:35 -0400

    Added more solutions to 8-23-23 coding challenge.

--- 8kyu/vowel-remover.js
+++ 8kyu/vowel-remover.js
@@ -23,4 +23,39 @@ function shortcut(string){
 
 
 
-  
+  function shortcut(str) {
+    return str.split('').filter(function(e) {
+      return ['a', 'e', 'i', 'o', 'u'].indexOf(e) == -1 
+    }).join('')
+  }
+
+
+
+  function shortcut (string) {
+    return string
+      .split('')
+      .filter(str => !'aeiou'.includes(str))
+      .join('')
+  }
+
+
+  const shortcut = string => string.replace( /[euioa]/g, '' );
+
+
+
+  const shortcut = string => string.replace(/[aeiou]/g, '');
+
+
+
+  function shortcut(string) {
+    return string.replace(/[aeiou]+/g, '')
+  }
+
+
+  const shortcut = (s) => s.replace(/[aeiou]/g,'');
+
+
+
+  function shortcut(s){
+    return s.replace(/[aeiou]/g,"")
+  }
