7b26699666d4b0cad789a39ea5564deb33460075
Charles Dobson <charlesmdobsonjr@gmail.com>
2023-09-01 18:40:32 -0400

    Added new challenge to 9-1-23 coding challenge.

---
+++ 8kyu/welcome-to-the-city.js
@@ -0,0 +1,49 @@
+// Create a method that takes as input a name, city, and state to welcome a person. Note that name will be an array consisting of one or more values that should be joined together with one space between each, and the length of the name array in test cases will vary.
+
+// Example:
+
+// ['John', 'Smith'], 'Phoenix', 'Arizona'
+// This example will return the string Hello, John Smith! Welcome to Phoenix, Arizona!
+
+
+// Solutions
+
+function sayHello( name, city, state ) {
+    return `Hello, ${name.join(' ')}! Welcome to ${city}, ${state}!`
+    }
+
+
+
+    function sayHello( name, city, state ) {
+        return 'Hello, ' + name.join(' ') + '! Welcome to ' + city + ', ' + state + '!';
+      }
+
+
+    
+      const sayHello = ( name, city, state ) => `Hello, ${name.join(' ')}! Welcome to ${city}, ${state}!`
+
+
+
+      function sayHello( name, city, state ) {
+        let fullName = name.join(" ");  
+        return `Hello, ${fullName}! Welcome to ${city}, ${state}!`;
+      }
+
+
+      sayHello = (name, city, state) => `Hello, ${name.join(' ')}! Welcome to ${city}, ${state}!`;
+
+
+
+      function sayHello(name, city, state) {
+        const fullName = name.join(" ");
+        
+        return `Hello, ${fullName}! Welcome to ${city}, ${state}!`;
+      }
+
+
+      function sayHello( name, city, state ) {
+        return 'Hello, ' + name.join(' ') + '! ' + 'Welcome to ' + city + ', ' + state + '!';
+    }
+
+
+    let sayHello = (name, city, state) => `Hello, ${name.join(' ')}! Welcome to ${city}, ${state}!`;
