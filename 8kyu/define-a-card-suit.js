1c4b9841f2c59c24692f74cb1b14c19bb8f9acc9
Charles Dobson <charlesmdobsonjr@gmail.com>
2023-09-04 01:23:26 -0400

    Added coding challenge to 9-04-23 code challenge.

---
+++ 8kyu/define-a-card-suit.js
@@ -0,0 +1,22 @@
+// You get any card as an argument. Your task is to return the suit of this card (in lowercase).
+
+// Our deck (is preloaded):
+
+// ('3♣') -> return 'clubs'
+// ('3♦') -> return 'diamonds'
+// ('3♥') -> return 'hearts'
+// ('3♠') -> return 'spades'
+
+// Solutions
+
+function defineSuit(card) {
+    const s = {
+      "♣": "clubs",
+      "♠": "spades",
+      "♦": "diamonds",
+      "♥": "hearts"
+    }
+    return s[card.charAt(card.length - 1)]
+  }
+
+
