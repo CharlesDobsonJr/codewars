9a3a2f7aba9d049b67603e8c67ff3958e0ec86ca
Charles Dobson <charlesmdobsonjr@gmail.com>
2023-09-09 02:05:27 -0400

    Added more solutions to 9-9-23 code challenge.

--- 8kyu/grasshopper-summation.js
+++ 8kyu/grasshopper-summation.js
@@ -26,3 +26,61 @@ var summation = function (num) {
   }
 
 
+  var summation = function (num) {
+    var sum = 0;
+    for(var i=0; i<=num;i++){
+      sum+=i
+    }
+    return sum;
+  }
+
+
+  var summation = function f(num) {
+    return num ? num + f(num-1) : 0;
+  }
+
+  
+  var summation = function (num) {
+    return num * (num+1) / 2;
+  }
+
+
+  var summation = function (num) {
+    let sum = 0;
+    
+    for (let i = 1; i <= num; i++) {
+      sum += i;
+    }
+    
+    return sum;
+  }
+
+
+  var summation = function (num) {
+    let sum = 0
+    for (let i = 0; i <= num; i++) {
+      sum += i
+    }
+    return sum
+   }
+
+
+
+   var summation = function (num) {
+    let result = 0;
+    for (let i = 1; i <= num; i++) {
+      result += i
+    }
+    return result;
+  }
+
+
+  var summation = function (num) {
+    let total = 0;
+    for (let i = 1; i <= num; i++){
+      total += i;
+    }
+    return total;
+  }
+
+  
