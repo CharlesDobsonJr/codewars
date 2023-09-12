50fbb65f82256b3d2f9c30e19b1a1b185d3b56bc
Charles Dobson <charlesmdobsonjr@gmail.com>
2023-09-11 23:03:42 -0400

    Added a solution to 9-11-23 code challenge.

--- 8kyu/counting-sheep.js
+++ 8kyu/counting-sheep.js
@@ -19,3 +19,15 @@ function countSheeps(arrayOfSheeps) {
   }
 
 
+  function countSheeps(arrayOfSheep) {
+    // TODO May the force be with you
+    var num = 0;
+    
+    for(var i = 0; i < arrayOfSheep.length; i++)
+      if(arrayOfSheep[i] == true)
+        num++;
+        
+    return num;
+  }
+
+
