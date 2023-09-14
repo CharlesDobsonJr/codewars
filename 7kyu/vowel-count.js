6939e97aa4079782428326e0de8465d63684d68b
Charles Dobson <charlesmdobsonjr@gmail.com>
2023-09-13 23:14:25 -0400

    Added solution to 9-13-23 code challenge.

--- 7kyu/vowel-count.js
+++ 7kyu/vowel-count.js
@@ -11,3 +11,19 @@ function getCount(str) {
   }
 
 
+  function getCount(str) {
+    var vowelsCount = 0;
+    var vowels = ["a","e","i","o","u"];
+    for(var i = 0;i < str.length;i++){
+      for(var j=0;j<vowels.length;j++){
+        if(str[i] === vowels[j]){
+          vowelsCount++;
+        }
+      }
+    }
+    
+    return vowelsCount;
+  }
+
+
+  
