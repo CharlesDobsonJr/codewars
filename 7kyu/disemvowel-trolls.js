09a530ff913eaa625505a1b88416ed1603746bd7
Charles Dobson <charlesmdobsonjr@gmail.com>
2023-09-15 02:23:01 -0400

    Added another solution to 9-14-23 code challenge.

--- 7kyu/disemvowel-trolls.js
+++ 7kyu/disemvowel-trolls.js
@@ -36,3 +36,18 @@ function disemvowel(str) {
   }
 
 
+  const disemvowel = (str) => {
+	const vowels = ['a', 'e', 'i', 'o', 'u', 'A', 'E', 'I', 'O', 'U'];
+	let newStr = '';
+	for (let i = 0; i <= str.length; i++) {
+		let char = str.charAt(i);
+		if (vowels.indexOf(char) == -1) {
+			newStr += char;
+		}
+	}
+		return newStr;
+		
+	
+};
+
+
