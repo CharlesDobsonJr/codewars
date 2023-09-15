f7182488135b29707382de81445af0f8d050a6b8
Charles Dobson <charlesmdobsonjr@gmail.com>
2023-09-15 02:23:51 -0400

    Added another solution to 9-14-23 code challenge.

--- 7kyu/disemvowel-trolls.js
+++ 7kyu/disemvowel-trolls.js
@@ -51,3 +51,12 @@ function disemvowel(str) {
 };
 
 
+function disemvowel(str) {
+    var vowels = ['a', 'e', 'i', 'o', 'u'];
+    
+    return str.split('').filter(function(el) {
+      return vowels.indexOf(el.toLowerCase()) == -1;
+    }).join('');
+  }
+
+
