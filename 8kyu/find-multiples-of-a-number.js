a94c599680efd965b15d1dff319834604cabbce0
Charles Dobson <charlesmdobsonjr@gmail.com>
2023-09-17 16:27:38 -0400

    Added a solution to 9-17-23 coding challenge.

--- 8kyu/find-multiples-of-a-number.js
+++ 8kyu/find-multiples-of-a-number.js
@@ -19,3 +19,10 @@ function findMultiples(int,limit){
   }
 
 
+  function findMultiples(int,limit){
+    let multiples = [];
+    for(let i = int; i <= limit; i += int){ multiples.push(i); }
+    return multiples;
+  }
+
+
