487070286e1c5e25885873a660c78d289dbd769e
Charles Dobson <charlesmdobsonjr@gmail.com>
2023-09-18 16:42:39 -0400

    Added code kyu 9-18-23 to code challenge.

---
+++ 8kyu/you-cant-code-under-pressure-no-1.js
@@ -0,0 +1,23 @@
+// Code as fast as you can! You need to double the integer and return it.
+
+// Solutions
+
+function doubleInteger(i) {
+    // i will be an integer. Double it and return it.
+    return i * 2;
+  }
+
+
+  const doubleInteger = i => 2 * i;
+
+
+
+  function doubleInteger(i) {
+    // i will be an integer. Double it and return it.
+    return i+i;
+  }
+
+
+  const doubleInteger = i => i*2
+
+
