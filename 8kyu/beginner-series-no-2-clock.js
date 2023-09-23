5eb78934744febc491ec312ae20d6be6338fa1e4
Charles Dobson <charlesmdobsonjr@gmail.com>
2023-09-23 16:58:45 -0400

    Added solution to 9-23-23 code challenge.

--- 8kyu/beginner-series-no-2-clock.js
+++ 8kyu/beginner-series-no-2-clock.js
@@ -43,3 +43,12 @@ function past(h, m, s){
   }
 
 
+  function past(h, m, s){
+    const setTime = new Date().setHours(h, m, s);
+    const midnight = new Date().setHours(0, 0, 0);
+    
+    return Math.round(setTime - midnight);
+  }
+
+
+  
