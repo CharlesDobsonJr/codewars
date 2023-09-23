0f2630c228e58e8986968224aedaddcdb0962c24
Charles Dobson <charlesmdobsonjr@gmail.com>
2023-09-23 17:00:37 -0400

    Added more solutions to 9-23-23 code challenge.

--- 8kyu/beginner-series-no-2-clock.js
+++ 8kyu/beginner-series-no-2-clock.js
@@ -51,4 +51,33 @@ function past(h, m, s){
   }
 
 
-  
+  function past(h, m, s){
+    return h * 3600000 + m * 60000  + s * 1000;
+  }
+
+
+  function past(h, m, s){
+    return (((h*60) + m)*60 + s)*1000;
+  }
+
+
+  function past(h, m, s){
+    return (h*60*60 + m*60 + s) * 1000;
+  }
+
+
+  const past = (h, m, s) => h * 3600000 + m * 60000 + s * 1000;
+
+
+
+  function past(h, m, s){
+    return s*1000 + m*60000 + h*3600000
+   }
+
+
+   function past(h, m, s){
+    return h * 60 * 60 * 1000 + m * 60 * 1000 + s * 1000;
+  }
+
+
+  const past = (h, m, s) => (h * 3600 + m * 60 + s) * 1000;
