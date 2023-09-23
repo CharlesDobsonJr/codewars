17e5a8ea4fd86d12e44dc11d3a7c05a5f94fe879
Charles Dobson <charlesmdobsonjr@gmail.com>
2023-09-23 16:58:00 -0400

    Added solution to 9-23-23 code challenge.

--- 8kyu/beginner-series-no-2-clock.js
+++ 8kyu/beginner-series-no-2-clock.js
@@ -34,3 +34,12 @@ function past(h, m, s){
   }
 
 
+  function past(h, m, s){
+    var miliseconds = 0;
+    miliseconds = miliseconds + s * 1000;
+    miliseconds = miliseconds + m * 60000;
+    miliseconds = miliseconds + h * 3600000;
+    return miliseconds;
+  }
+
+
