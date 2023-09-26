2b7bb65b6e5d37b463545d2a0b82c2f3fd2e76c5
Charles Dobson <charlesmdobsonjr@gmail.com>
2023-09-26 17:17:32 -0400

    Added more solutions to 9-26-23 code challenge.

--- 7kyu/sum-of-two-lowest-positive-integers.js
+++ 7kyu/sum-of-two-lowest-positive-integers.js
@@ -24,3 +24,39 @@ function sumTwoSmallestNumbers(numbers){
   };
 
 
+  function sumTwoSmallestNumbers(numbers) {  
+    var min = Number.MAX_SAFE_INTEGER;
+    var secondMin = Number.MAX_SAFE_INTEGER;
+    
+    var n;
+    for (i = 0; i < numbers.length; i++) {
+      n = numbers[i];
+        if(n < min) {
+          secondMin = min;
+        min = n;
+      } else if( n < secondMin ) {
+          secondMin = n;
+      }
+    }
+    
+    return min + secondMin;
+  }
+
+
+  function sumTwoSmallestNumbers(numbers) {  
+    numbers.sort(function (a, b) { 
+      return a - b;
+    });
+    return numbers[0] + numbers[1];
+  };
+
+
+  function sumTwoSmallestNumbers(numbers) {  
+    return numbers.sort((a,b) => a-b).slice(0,2).reduce((a,b)=>a+b);
+  };
+
+
+  function sumTwoSmallestNumbers(numbers) {  
+    var sorted = numbers.sort(function(a,b) { return a - b; });
+    return sorted[0] + sorted[1];
+  }; 
