dcff98a3e83132b4b3c29dd9058c4db087b93054
Charles Dobson <charlesmdobsonjr@gmail.com>
2023-10-02 17:32:50 -0400

    Added coding challenge to 10-2-23 code challenge.

---
+++ 7kyu/sum-of-odd-numbers.js
@@ -0,0 +1,69 @@
+// Given the triangle of consecutive odd numbers:
+
+//              1
+//           3     5
+//        7     9    11
+//    13    15    17    19
+// 21    23    25    27    29
+// ...
+// Calculate the sum of the numbers in the nth row of this triangle (starting at index 1) e.g.: (Input --> Output)
+
+// 1 -->  1
+// 2 --> 3 + 5 = 8
+
+// Solutions
+
+function rowSumOddNumbers(n) {
+    return Math.pow(n, 3);
+  }
+
+
+
+  function rowSumOddNumbers(n)
+  {
+    /* The rows' start numbers are Hogben's centered polygonal numbers:
+       1, 3, 7, 13, 21, 31, 43 = b[n] = n^2 - n + 1.
+       <https://oeis.org/A002061>
+       
+       The sum of one row is given by:
+       s[n] = n^2 + n(b[n] - 1).
+       <https://www.quora.com/What-is-the-sum-of-n-consecutive-odd-integers/answer/Xavier-Dectot>
+       
+       Inline b[n]:
+       s[n] = n^2 + n(n^2 - n + 1 - 1)
+            = n^2 + n(n^2 - n)
+            = n^2 + n^3 - n^2
+            = n^3
+       ... oh. */
+    return n * n * n;
+  }
+
+
+
+  function rowSumOddNumbers(n) {
+	return n*n*n
+}
+
+
+let rowSumOddNumbers = n => n ** 3;
+
+
+function rowSumOddNumbers(n) {
+	return n**3
+}
+
+
+const rowSumOddNumbers = n => Math.pow(n, 3);
+
+
+
+const rowSumOddNumbers = n => n * n * n;
+
+
+const rowSumOddNumbers=n=>n**3;
+
+
+
+function rowSumOddNumbers(n) {return n**2*n}
+
+
