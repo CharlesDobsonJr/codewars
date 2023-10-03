a2602f1bb361230984449a82f129be4b8a4a24ce
Charles Dobson <charlesmdobsonjr@gmail.com>
2023-10-03 19:11:44 -0400

    Added more solutions to 10-3-23 coding challenge.

--- 7kyu/odd-or-even.js
+++ 7kyu/odd-or-even.js
@@ -32,3 +32,45 @@ function oddOrEven(arr) {
 
 
 
+  const oddOrEven = a => a.reduce((s,n)=> s + n,0)%2 == 0 ? "even" : "odd";
+
+
+
+  const oddOrEven = a => a.reduce((a, b) => a + b, 0) % 2 === 0 ? 'even' : 'odd';
+
+
+
+  const oddOrEven = (arr) => arr.reduce((a,i) => a + i, 0) % 2 ? 'odd' : 'even'
+
+
+
+  function oddOrEven(array) {
+    if(array.length == 0){
+      return 'even'
+    }
+    var sum = array.reduce(add, 0)
+    function add(x, y){
+      return x + y
+    }
+    
+    if(sum % 2 == 0){return 'even'}
+    else{return 'odd'}
+ }
+
+
+ function oddOrEven(array) {
+  return array.reduce((a,b)=>a+b,0)%2===0?"even":"odd";
+}
+
+
+function oddOrEven(array){
+  return array.reduce((a,b) => a+b,0) % 2 ? 'odd' : 'even';
+}
+
+
+function oddOrEven(array) {
+  return array.reduce((a, b) => a +b, 0) % 2 == 0 ? 'even' : 'odd';
+}
+
+
+
