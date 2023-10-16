116ff89af8c0c5560bfe22e9b8fa6a4249bd812b
Charles Dobson <charlesmdobsonjr@gmail.com>
2023-10-16 19:28:38 -0400

    Added code challenge 10-16-23 to coding challenge.

---
+++ 6kyu/create-phone-number.js
@@ -0,0 +1,61 @@
+// Write a function that accepts an array of 10 integers (between 0 and 9), that returns a string of those numbers in the form of a phone number.
+
+// Example
+// createPhoneNumber([1, 2, 3, 4, 5, 6, 7, 8, 9, 0]) // => returns "(123) 456-7890"
+// The returned format must be correct in order to complete this challenge.
+
+// Don't forget the space after the closing parentheses!
+
+
+// Solutions
+
+function createPhoneNumber(numbers){
+    var format = "(xxx) xxx-xxxx";
+    
+    for(var i = 0; i < numbers.length; i++)
+    {
+      format = format.replace('x', numbers[i]);
+    }
+    
+    return format;
+  }
+
+
+  function createPhoneNumber(numbers){
+    numbers = numbers.join('');
+    return '(' + numbers.substring(0, 3) + ') ' 
+        + numbers.substring(3, 6) 
+        + '-' 
+        + numbers.substring(6);
+  }
+
+
+  function createPhoneNumber(numbers){
+    return numbers.join('').replace(/(...)(...)(.*)/, '($1) $2-$3');
+  }
+
+
+  function createPhoneNumber(numbers){
+    return numbers.join('').replace(/(\d{3})(\d{3})(\d{4})/,'($1) $2-$3');
+  }
+
+
+  function createPhoneNumber(numbers){
+    return '(' + numbers.slice(0,3).join('') + ') ' + numbers.slice(3,6).join('') + '-' + numbers.slice(6).join('');
+  }
+
+
+
+  function createPhoneNumber(numbers){
+    return '(' + numbers[0] + numbers[1] + numbers[2] + ') ' + numbers[3] + numbers[4] + numbers[5] + '-' + numbers[6] + numbers[7] + numbers[8] + numbers[9];
+  }
+
+
+  function createPhoneNumber(numbers){
+    return `(${numbers[0]}${numbers[1]}${numbers[2]}) ${numbers[3]}${numbers[4]}${numbers[5]}-${numbers[6]}${numbers[7]}${numbers[8]}${numbers[9]}`
+  }
+
+
+  function createPhoneNumber(numbers){
+    return `(${numbers.slice(0, 3).join('')}) ${numbers.slice(3, 6).join('')}-${numbers.slice(6).join('')}`;
+  }
