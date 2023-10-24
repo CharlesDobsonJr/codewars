10cfb23a159b7d2dfa5a91bc17af2dea83f9e1a4
Charles Dobson <charlesmdobsonjr@gmail.com>
2023-10-24 04:28:00 -0400

    added new 8kyu kata called Multiply

---
+++ 8kyu/multiply.js
@@ -0,0 +1,20 @@
+// This code does not execute properly. Try to figure out why.
+
+//Solutions
+
+function multiply(a, b){
+    return a * b
+  }
+
+
+  const multiply = (a, b) => a * b;
+
+
+  multiply = (a, b) => a * b;
+
+
+  multiply = function (a, b) {
+    return a * b;
+  }
+
+  
