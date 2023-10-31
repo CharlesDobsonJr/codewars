f27f7cfe5811e32266be9278288325c4facbc9d0
Charles Dobson <charlesmdobsonjr@gmail.com>
2023-10-31 19:25:28 -0400

    Added a couple of solutions to 10-31-23 code challenge.

--- 6kyu/replace-with-alphabet-position.js
+++ 6kyu/replace-with-alphabet-position.js
@@ -23,3 +23,18 @@ function alphabetPosition(text) {
 
 
 
+  let alphabetPosition = (text) => text.toUpperCase().replace(/[^A-Z]/g, '').split('').map(ch => ch.charCodeAt(0) - 64).join(' ');
+
+
+  function alphabetPosition(text) {
+    var alphabet = 'abcdefghijklmnopqrstuvwxyz'.split('');
+    return text.toLowerCase().split('')
+    .filter( letter => {
+      let index = alphabet.indexOf(letter);
+      return index > -1;
+    }  )
+    .map( letter => alphabet.indexOf(letter) + 1 )
+    .join(' ')
+  }
+
+
