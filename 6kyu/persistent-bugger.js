3457d4b8caaaef20bb26f15f965c7a4b4578909e
Charles Dobson <charlesmdobsonjr@gmail.com>
2023-11-01 18:25:16 -0400

    Added a long solution to 11-1-23 code challenge.

--- 6kyu/persistent-bugger.js
+++ 6kyu/persistent-bugger.js
@@ -37,3 +37,42 @@ function persistence(num) {
  }
 
 
+/**
+ * This function will calculate the multiplication of the digit of the number 
+ * given as parameter
+ */
+function multiplyDigits( num ) {
+	// If have just 1 digit number, we just return it
+	if( num < 10 ) {
+  	return num;
+  // If more than one digit, will be first digit * recursive call to this function
+  }else {
+    let firstDigit = Math.floor( num / 10 );
+    return ( num - firstDigit * 10 ) * 
+    	multiplyDigits(firstDigit);
+  }
+}
+
+/**
+ * This function will count on the number of times we have to call multiply
+ * till we don't have any more times to call ( we just have one digit ) 
+ */
+function count( num, times = 0 ) {
+  if( num < 10 ) {
+  	return times;
+  }else {
+  	// 999 = 9 * 9 * 9 = 729, ...
+  	return count( multiplyDigits( num ), ++times );
+  }
+}
+/**
+ * Just the public function we call in order to count the number of times
+ * we have to multiply the digits of the param number till we have just 
+ * one digit as result 
+ */
+function persistence( num ) {
+  return count( num );
+}
+
+
+
