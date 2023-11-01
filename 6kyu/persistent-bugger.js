b280aa8d3154619e5392c5efd5e8a036c3dabd63
Charles Dobson <charlesmdobsonjr@gmail.com>
2023-11-01 18:27:44 -0400

    Added another solution to 11-1-23 code challenge.

--- 6kyu/persistent-bugger.js
+++ 6kyu/persistent-bugger.js
@@ -96,4 +96,22 @@ function persistence(num, cnt=0) {
  }
 
 
- 
+ function persistence(num) {
+  
+    function multiply(n) {
+      return n.reduce(function(a, b) {
+        return a * b;
+      });
+    }
+  
+     var count = 0;
+     while(num.toString().length > 1) {
+       num = num.toString().split("");
+       num = multiply(num);
+       count++
+     }
+     return count;
+  }
+
+
+  
