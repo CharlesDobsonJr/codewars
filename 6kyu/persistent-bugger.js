79268242b220ceb6e495d47bfdcdde2d44c46f04
Charles Dobson <charlesmdobsonjr@gmail.com>
2023-11-01 18:23:26 -0400

    Added a solution to 11-1-23 code challenge.

--- 6kyu/persistent-bugger.js
+++ 6kyu/persistent-bugger.js
@@ -22,3 +22,10 @@ function persistence(num) {
  }
 
 
+ const persistence = num => {
+    return `${num}`.length > 1 
+      ? 1 + persistence(`${num}`.split('').reduce((a, b) => a * +b)) 
+      : 0;
+  }
+
+
