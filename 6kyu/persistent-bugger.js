787a884b9d958993a7e5821c62600f1b89067786
Charles Dobson <charlesmdobsonjr@gmail.com>
2023-11-01 18:26:46 -0400

    Added another solution to 11-1-23 code challenge.

--- 6kyu/persistent-bugger.js
+++ 6kyu/persistent-bugger.js
@@ -85,3 +85,15 @@ function persistence(num, cnt=0) {
 
 
 
+  function persistence(num) {
+    let result = 0,
+        str = String(num)
+    while (str.length > 1) {
+      str = String([...str].reduce((prev,curr) => prev * curr))
+      result++
+    }
+   return result
+ }
+
+
+ 
