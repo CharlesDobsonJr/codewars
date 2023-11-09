6f33e79ee9913304485e13cf0672eee7a89cc60f
Charles Dobson <charlesmdobsonjr@gmail.com>
2023-11-09 00:38:48 -0500

    Added another solution to 11-9-23 code challenge.

--- 8kyu/short.long.short.js
+++ 8kyu/short.long.short.js
@@ -20,3 +20,28 @@ function solution(a, b) {
   }
 
 
+// Using if...else, explicit block syntax
+function solution(a, b) {
+    if (a.length < b.length) {
+        return a + b + a;
+    } else {
+        return b + a + b;
+    }
+}
+
+// Using if..., implicit else, compressed syntax
+function solution(a, b) {
+    if (a.length < b.length) return a + b + a;
+    return b + a + b;
+}
+
+// Using ternary conditional operator
+function solution(a, b) {
+    return (a.length < b.length) ? (a + b + a) : (b + a + b);
+}
+
+// ES6 arrow function
+var solution = (a, b) =>
+    (a.length < b.length) ? (a + b + a) : (b + a + b);
+
+
